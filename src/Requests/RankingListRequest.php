845e980fe074b38a7208fcd224b46bc552960906
MuBai <better.man@aliyun.com>
2020-04-05 10:42:57 +0800

    各大榜单API升级为v1.1.2

--- src/Requests/RankingListRequest.php
+++ src/Requests/RankingListRequest.php
@@ -34,13 +34,11 @@ class RankingListRequest implements Request
      *
      * @var string
      */
-    public $version = 'v1.1.0';
+    public $version = 'v1.1.2';
 
     private $rankType;      // 榜单类型 1.实时榜 2.全天榜 3.热推榜（热推榜分类无效）4.复购榜
 
-    private $cid;           // 大淘客一级类目id 默认为1
-                            // 1 -女装，2 -母婴，3 -美妆，4 -居家日用，5 -鞋品，6 -美食，7 -文娱车品，8 -数码家电，9 -男装，
-                            // 10 -内衣，11 -箱包，12 -配饰，13 -户外运动，14 -家装家纺
+    private $cid;           // 大淘客一级类目id 仅对实时榜单、全天榜单有效
 
     private $apiParams = [];
 
